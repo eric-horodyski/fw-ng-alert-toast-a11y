62c14597a5e2074c8f5f530e263e62102dfe98ac
Eric Horodyski <horodyski@ionic.io>
2021-08-26 09:01:48 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fw-ng-alert-toast-a11y',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
