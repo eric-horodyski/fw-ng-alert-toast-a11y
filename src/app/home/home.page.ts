4f560d2691b2947ff83f6aba018f2c9831a7e381
Eric Horodyski <horodyski@ionic.io>
2021-08-26 10:18:55 -0400

    show alert + toast

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(
+    public toastController: ToastController,
+    public alertController: AlertController
+  ) {}
 
-  constructor() {}
+  async ngOnInit() {
+    this.presentToast();
+    this.presentAlert();
+  }
 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your settings have been saved.',
+    });
+    toast.present();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
